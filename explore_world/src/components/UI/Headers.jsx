42fefbb14b52a62fec9eb55e3d79cef55ffe6a78
Syed Ekramul Khayer Sayem <168598535+Syed-Sayem@users.noreply.github.com>
2025-06-03 02:13:50 +0530

    World-explore
    This website provides comprehensive information on 295 countries, covering key details such as geography, population, and culture. It serves as a one-stop platform for users to explore global country data easily.

---
+++ explore_world/src/components/UI/Headers.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Headers = () => {
+  const [show, setshow] = useState(false);
+
+  const handleButtonToggle = () => {
+    return setshow(!show);
+  };
+  return (
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="Logo">
+            <NavLink to="/">
+              <h1>WorldTrip</h1>
+            </NavLink>
+          </div>
+          <nav className={show ? "menu-mobile" : "menu-web"}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/country">Country</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="ham-menu">
+            ,
+            <button onClick={handleButtonToggle}>
+              <GiHamburgerMenu />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Headers;
