98fb534f108f273115bf80f3f788ed47b1c3edff
Syed Ekramul Khayer Sayem <168598535+Syed-Sayem@users.noreply.github.com>
2025-11-20 13:23:12 +0600

    Initial project setup

--- explore_world/src/components/UI/HeroSection.jsx
+++ explore_world/src/components/UI/HeroSection.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
-     <main className='hero-section main'>
-          <div className='container grid grid-two-cols'>
-            <div className='hero-content'>
-              <h1 className='heading-xl'>
-                Explore the world, One Country at a Time.
-              </h1>
-              <p className='paragraph'>
-                Discover the history, culture, and beauty of every nation. Sort, search, and filter through countries fo find the details you.
-    
-              </p>
-              <button className='btn btn-darken btn-inline bg-white-box'>
-                Start Exploring <FaLongArrowAltRight/>
-              </button>
-            </div>
-            <div className='hero-image'>
-              <img src="/images/world.png" alt="world beauty" className='banner-image' />
-            </div>
-          </div>
-        </main>
-  )
-}
+    <main className="hero-section main">
+      <div className="container grid grid-two-cols">
+        <div className="hero-content">
+          <h1 className="heading-xl">
+            Explore the world, One Country at a Time.
+          </h1>
+          <p className="paragraph">
+            Discover the history, culture, and beauty of every nation. Sort,
+            search, and filter through countries fo find the details you.
+          </p>
+          <NavLink to="/country">
+            <button className="btn btn-blue btn-inline bg-white-box">
+              Start Exploring <FaLongArrowAltRight />
+            </button>
+          </NavLink>
+        </div>
+        <div className="hero-image">
+          <img
+            src="/images/world.png"
+            alt="world beauty"
+            className="banner-image"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
